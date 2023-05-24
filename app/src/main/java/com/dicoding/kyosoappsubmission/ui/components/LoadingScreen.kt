dae92262e67d069528d1e20887576c2a0b5e0faa
bintangkhd <bintangkhadafi2772@gmail.com>
2023-05-24 19:52:01 +0700

    Create Loading Screen

---
+++ app/src/main/java/com/dicoding/kyosoappsubmission/ui/components/LoadingScreen.kt
@@ -0,0 +1,25 @@
+package com.dicoding.kyosoappsubmission.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoadingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Preview
+@Composable
+fun LoadingScreenPreview() {
+    LoadingScreen()
+}
